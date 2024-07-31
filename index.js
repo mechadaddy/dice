048fc7dc095af10d1fe3c02b605fe6b3f422f717
mechadaddy <164714633+mechadaddy@users.noreply.github.com>
2024-07-31 00:47:15 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+function rollDice() {
+
+    var randomNum1 = Math.floor(Math.random()*6) +1;
+    var randomNum2 = Math.floor(Math.random()*6) +1;
+
+    if (randomNum1 != randomNum2) {
+        document.querySelectorAll("img")[0].setAttribute("src", `./images/dice${randomNum1}.png`);
+
+        document.querySelectorAll("img")[1].setAttribute("src", `./images/dice${randomNum2}.png`);
+
+        if (randomNum1 > randomNum2) {
+            document.querySelector("h1").textContent="🚩Player 1 Wins";
+        } else if (randomNum1 < randomNum2) {
+            document.querySelector("h1").textContent="Player 2 Wins 🚩";
+        } else {
+            console.log("huh?");
+        }
+
+    } else {
+        rollDice();
+    }
+
+
+}
+
+rollDice();
